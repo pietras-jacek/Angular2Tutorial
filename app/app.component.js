53ae6d9141107522ed5695431d93b55fc413515d
Jacek Pietras <jaxilus@gmail.com>
2016-04-14 11:51:17 +0200

    przerobiony rozdział 3

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', './hero-detail.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,20 +10,17 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
-    var Hero, AppComponent, HEROES;
+    var core_1, hero_detail_component_1;
+    var AppComponent, HEROES;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (hero_detail_component_1_1) {
+                hero_detail_component_1 = hero_detail_component_1_1;
             }],
         execute: function() {
-            Hero = (function () {
-                function Hero() {
-                }
-                return Hero;
-            }());
-            exports_1("Hero", Hero);
             AppComponent = (function () {
                 function AppComponent() {
                     this.title = "Tour of Heroes";
@@ -35,7 +32,8 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n    <h1>{{title}}</h1>\n    <h2>My Heroes</h2>\n    <ul class=\"heroes\">\n\t\t<li *ngFor=\"#hero of heroes\" \n\t\t\t[class.selected]=\"hero === selectedHero\"\n\t\t\t(click)=\"onSelect(hero)\">\n\t\t\t<span class=badge>{{hero.id}}</span> {{hero.name}}\n\t\t</li>\n    </ul>\n    <div *ngIf=\"selectedHero\">\n\t\t<h2>{{selectedHero.name}} details!</h2>\n\t\t<div><label>id: </label>{{selectedHero.id}}</div>\n    \t<div>\n    \t\t<label>name: </label>\n\t\t\t<input [(ngModel)]=\"selectedHero.name\" placeholder=\"name\"/>\n    \t</div>\n    </div>\n    "
+                        template: "\n    <h1>{{title}}</h1>\n    <h2>My Heroes</h2>\n    <ul class=\"heroes\">\n\t\t<li *ngFor=\"#hero of heroes\" \n\t\t\t[class.selected]=\"hero === selectedHero\"\n\t\t\t(click)=\"onSelect(hero)\">\n\t\t\t<span class=badge>{{hero.id}}</span> {{hero.name}}\n\t\t</li>\n    </ul>\n   \t<my-hero-detail [hero]=\"selectedHero\"></my-hero-detail>\n    ",
+                        directives: [hero_detail_component_1.HeroDetailComponent]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
